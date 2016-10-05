6a38958f1789c31b6bfad07d10b624f5a1afc3b8
Josh Sephton <josh.sephton@gmail.com>
2016-10-05 11:58:44 +0100

    feat: toggling setup configures wlan interface

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import getopt
 import gitlab
+import logging
 import threading
 import time
+import sys
+
+from setup import Setup
 
 class PeepingTom:
 
@@ -10,44 +14,37 @@ class PeepingTom:
 		self.project_id = arguments.project_id
 		self.client = gitlab.Gitlab('http://gitlab.com', self.private_token)
 		self.setup_mode = False
+		self.setup = Setup()
+		logging.basicConfig(level=arguments.log_level)
 
 	def fetch_build_status(self, project_id):
 		latest_commit = self.client.project_commits.list(project_id=project_id, page=0, per_page=1)[0]
 		latest_build = latest_commit.builds(page=0, per_page=1)[0];
-
 		print(latest_build.status)
 
 	def execute(self):
 		threading.Thread(target=self.start_status_loop).start()
-		threading.Thread(target=self.start_setup_loop).start()
+		threading.Thread(target=self.setup.await_input).start()
 		while(1):
 			time.sleep(1)
 
 	# Fetch the project status every 30 seconds
 	def start_status_loop(self):
 		while (1):
-			if self.setup_mode is not True:
+			if self.setup.setup_mode is not True:
 				self.fetch_build_status(self.project_id)
 				time.sleep(5)
-
-	# Listen out for input. This will be replaced
-	# by GPIO input'
-	def start_setup_loop(self):
-		while(1):
-			s = raw_input()
-			self.setup_mode = not self.setup_mode
-			print "Setup Mode: %s" % self.setup_mode
-			time.sleep(1)
 
 class PeepingTomArgs:
 
 	def __init__(self):
 		self.private_token = ''
 		self.project_id = ''
+		self.log_level = logging.WARN
 
 	def parse_arguments(self, argv):
 		try:
-			opts, args = getopt.getopt(argv,"ht:p:")
+			opts, args = getopt.getopt(argv, "ht:p:",["log="])
 		except getopt.GetoptError:
 			self.display_help()
 			sys.exit(2)
@@ -60,6 +57,8 @@ class PeepingTomArgs:
 				self.private_token = arg
 			elif opt in ("-p"):
 				self.project_id = arg
+			elif opt in ("--log"):
+				self.log_level = getattr(logging, arg.upper())
 
 		if self.private_token == '' or self.project_id == '':
 			self.display_help()
---
+++ setup.py
@@ -0,0 +1,43 @@
+import logging
+import os
+from subprocess import call
+import time
+
+class Setup:
+
+	def __init__(self):
+		self.setup_mode = True
+
+	# Listen out for input. This will be replaced
+	# by GPIO input'
+	def await_input(self):
+		while(1):
+			s = raw_input()
+			self.setup_mode = not self.setup_mode
+			if self.setup_mode == True:
+				self.__enter_setup()
+			else:
+				self.__exit_setup()
+			time.sleep(1)
+
+	def __enter_setup(self):
+		logging.info('Entering setup.')
+		self.__create_adhoc_network()
+
+	def __exit_setup(self):
+		logging.info('Exiting setup.')
+		self.__create_default_network()
+
+	def __create_default_network(self):
+		source = os.getcwd() + "/wifi-config/default.conf"
+		target = "/etc/networks/interfaces"
+		logging.info('Switching network configuration at %s for %s.', target, source)
+		# os.symlink(source, target)
+		# call(["dhclient", "wlan0"])
+
+	def __create_adhoc_network(self):
+		source = os.getcwd() + "/wifi-config/adhoc.conf"
+		target = "/etc/networks/interfaces"
+		logging.info('Switching network configuration at %s for %s.', target, source)
+		# os.symlink(source, target)
+		# call(["dhclient", "wlan0"])
