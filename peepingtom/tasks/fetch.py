90ec811d9cc5cc7594b10de221b31ff3ba15a563
Josh Sephton <josh.sephton@gmail.com>
2016-10-06 21:51:16 +0100

    fix: catch connection error and prompt user

--- peepingtom/tasks/fetch.py
+++ peepingtom/tasks/fetch.py
@@ -1,4 +1,5 @@
 import gitlab
+import logging
 import threading
 import time
 
@@ -16,12 +17,20 @@ class Fetch(threading.Thread):
 	def run(self):
 		while not self.should_exit.isSet():
 			time.sleep(3)
-			project = self.client.projects.get(self.project_id)
-			if project is not None:
-				latest_commit = self.client.project_commits.list(project_id=project.id, page=0, per_page=1)[0]
-				latest_build = latest_commit.builds(page=0, per_page=1)[0];
+			try:
+				project = self.client.projects.get(self.project_id)
+				if project is not None:
+					latest_commit = self.client.project_commits.list(project_id=project.id, page=0, per_page=1)[0]
+					latest_build = latest_commit.builds(page=0, per_page=1)[0];
+					self.lcd_lock.acquire()
+					self.lcd.clear()
+					self.lcd.message(project.path_with_namespace + ":\n" + latest_build.status)
+					time.sleep(2)
+					self.lcd_lock.release()
+			except gitlab.GitlabConnectionError:
+				logging.info("Could not connect to Gitlab.")
 				self.lcd_lock.acquire()
 				self.lcd.clear()
-				self.lcd.message(project.path_with_namespace + ":\n" + latest_build.status)
+				self.lcd.message("Connection\nError :(")
 				time.sleep(2)
 				self.lcd_lock.release()
