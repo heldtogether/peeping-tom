fd4f9f2d90ab4875221c8640f044bdfe7d0e8dd7
Josh Sephton <josh.sephton@gmail.com>
2016-10-07 08:50:40 +0100

    style: update whitespace

--- peepingtom/arguments.py
+++ peepingtom/arguments.py
@@ -2,37 +2,37 @@ import getopt
 import logging
 import sys
 
+
 class Arguments:
+    def __init__(self):
+        self.private_token = ''
+        self.project_id = ''
+        self.log_level = logging.WARN
+        self.debug = False
 
-	def __init__(self):
-		self.private_token = ''
-		self.project_id = ''
-		self.log_level = logging.WARN
-		self.debug = False
+    def parse_arguments(self, argv):
+        try:
+            opts, args = getopt.getopt(argv, "ht:p:", ["log=", "debug"])
+        except getopt.GetoptError:
+            self.display_help()
+            sys.exit(2)
 
-	def parse_arguments(self, argv):
-		try:
-			opts, args = getopt.getopt(argv, "ht:p:",["log=", "debug"])
-		except getopt.GetoptError:
-			self.display_help()
-			sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                self.display_help()
+                sys.exit()
+            elif opt in ("-t"):
+                self.private_token = arg
+            elif opt in ("-p"):
+                self.project_id = arg
+            elif opt in ("--log"):
+                self.log_level = getattr(logging, arg.upper())
+            elif opt in ("--debug"):
+                self.debug = True
 
-		for opt, arg in opts:
-			if opt == '-h':
-				self.display_help()
-				sys.exit()
-			elif opt in ("-t"):
-				self.private_token = arg
-			elif opt in ("-p"):
-				self.project_id = arg
-			elif opt in ("--log"):
-				self.log_level = getattr(logging, arg.upper())
-			elif opt in ("--debug"):
-				self.debug = True
+        if self.private_token == '' or self.project_id == '':
+            self.display_help()
+            sys.exit(2)
 
-		if self.private_token == '' or self.project_id == '':
-			self.display_help()
-			sys.exit(2)
-
-	def display_help(self):
-		print 'peeping-tom.py -t <private-token> -p <project_id>'
+    def display_help(self):
+        print 'peeping-tom.py -t <private-token> -p <project_id>'
