7aeb1405155030d30ed835ed6e314278f03dca94
Josh Sephton <josh.sephton@gmail.com>
2016-10-08 16:04:17 +0100

    feat: update gpio pins for lcd

--- peepingtom/io/lcd.py
+++ peepingtom/io/lcd.py
@@ -1,16 +1,15 @@
 try:
-    from Adafruit_CharLCD import Adafruit_CharLCD
+    import Adafruit_CharLCD
 except ImportError:
     import utils.mocklcd as Adafruit_CharLCD
 
 # Raspberry Pi pin configuration:
-lcd_rs = 27
-lcd_en = 22
-lcd_d4 = 25
-lcd_d5 = 24
-lcd_d6 = 23
-lcd_d7 = 18
-lcd_backlight = 4
+lcd_rs = 13
+lcd_en = 16
+lcd_d4 = 21
+lcd_d5 = 20
+lcd_d6 = 26
+lcd_d7 = 19
 
 # Define LCD column and row size for 16x2 LCD.
 lcd_columns = 16
@@ -20,4 +19,4 @@ lcd_rows = 2
 class LCD(Adafruit_CharLCD.Adafruit_CharLCD):
     def __init__(self):
         Adafruit_CharLCD.Adafruit_CharLCD.__init__(self, lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns,
-                                                   lcd_rows, lcd_backlight)
+                                                   lcd_rows)
--- peepingtom/io/utils/mocklcd.py
+++ peepingtom/io/utils/mocklcd.py
@@ -1,56 +1,56 @@
 import sys
 
+
 class Adafruit_CharLCD:
+    def __init__(self, rs, en, d4, d5, d6, d7, cols, lines, backlight=None,
+                 invert_polarity=True,
+                 enable_pwm=False,
+                 gpio=None,
+                 pwm=None,
+                 initial_backlight=1.0):
+        pass
 
-	def __init__(self, rs, en, d4, d5, d6, d7, cols, lines, backlight=None,
-					invert_polarity=True,
-					enable_pwm=False,
-					gpio=None,
-					pwm=None,
-					initial_backlight=1.0):
-		pass
+    def home(self):
+        pass
 
-	def home(self):
-		pass
+    def clear(self):
+        pass
 
-	def clear(self):
-		pass
+    def set_cursor(self, col, row):
+        pass
 
-	def set_cursor(self, col, row):
-		pass
+    def enable_display(self, enable):
+        pass
 
-	def enable_display(self, enable):
-		pass
+    def show_cursor(self, show):
+        pass
 
-	def show_cursor(self, show):
-		pass
+    def blink(self, blink):
+        pass
 
-	def blink(self, blink):
-		pass
+    def move_left(self):
+        pass
 
-	def move_left(self):
-		pass
+    def move_right(self):
+        pass
 
-	def move_right(self):
-		pass
+    def set_left_to_right(self):
+        pass
 
-	def set_left_to_right(self):
-		pass
+    def set_right_to_left(self):
+        pass
 
-	def set_right_to_left(self):
-		pass
+    def autoscroll(self, autoscroll):
+        pass
 
-	def autoscroll(self, autoscroll):
-		pass
+    def message(self, text):
+        print(text)
 
-	def message(self, text):
-		print(text)
+    def set_backlight(self, backlight):
+        pass
 
-	def set_backlight(self, backlight):
-		pass
+    def write8(self, value, char_mode=False):
+        pass
 
-	def write8(self, value, char_mode=False):
-		pass
-
-	def create_char(self, location, pattern):
-		pass
+    def create_char(self, location, pattern):
+        pass
